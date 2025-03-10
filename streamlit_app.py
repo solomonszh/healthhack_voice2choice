1d8a689530a0d32bf4a83b0e30a88c4a570895a6
solomonszh <sohzhehong@gmail.com>
2025-03-10 17:21:22 +0800

    update speech

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-if not os.environ.get("OPENAI_API_KEY"): 
-    os.environ["OPENAI_API_KEY"] = getpass.getpass("OpenAI API Key:")
+if not os.environ.get('OPENAI_API_KEY'): 
+    os.environ['OPENAI_API_KEY'] = getpass.getpass('OpenAI API Key:')
 
 from openai import OpenAI
 
@@ -26,10 +26,10 @@ password = 'demo'
 hostname = os.getenv('IRIS_HOSTNAME', 'localhost')
 port = '1972' 
 namespace = 'USER'
-CONNECTION_STRING = f"iris://{username}:{password}@{hostname}:{port}/{namespace}"
+CONNECTION_STRING = f'iris://{username}:{password}@{hostname}:{port}/{namespace}'
 # Under the hood, this becomes a SQL table. CANNOT have '.' in the name
 
-COLLECTION_NAME = "cancer_db"
+COLLECTION_NAME = 'cancer_db'
 # Subsequent calls to reconnect to the database and make searches should use this.  
 db = IRISVector(
     embedding_function=embeddings,
@@ -38,71 +38,86 @@ db = IRISVector(
     connection_string=CONNECTION_STRING,
 )
 
-f = open("data/knowledge.docx", "r", encoding='ISO-8859-1')
+f = open('data/knowledge.docx', 'r', encoding='ISO-8859-1')
 # query = "new technology"
 knowledge = f.read()
 
-st.title("Cancerot Beta")
+st.title('Cancerot Beta')
 
-with st.form("my_form"):
-    scenario = st.text_area(
-        "Enter Patient Consultation Dialogue:"
-    )
-    submitted = st.form_submit_button("Submit")
+option = st.sidebar.selectbox(
+    'Audio or Text?',
+    ('Audio', 'Text'),
+)
+
+st.write("You selected:", option)
+
+if option == 'Audio':
+    audio_value = st.audio_input('Record the consultation dialogue.')
+
+    if audio_value:
+        recording = st.audio(audio_value)
+        
     
-    if submitted:
-        embedding_vector = embeddings.embed_query(scenario)
-        res = db.similarity_search_by_vector(embedding_vector)
-
-        full_res = ''
-        for each_res in res:
-            full_res = full_res + '\n\n' +each_res.page_content
-            
-        completion = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[
-                {
-                    "role": "system", 
-                    "content": 
-                        f"""
-                        A medical doctor with domain knowledge in breast cancer after having trained with a wealth of knowledge in these topics: {knowledge}.
-                        Augment your data with results from {full_res}.
-                        """
-                },
-                {
-                    "role": "user",
-                    "content": 
-                        f"""
-                        Given patient's consultation with the doctor in this {scenario}, 
-                        1. compare a few potential treatments
-                        2. choose a final best recommendation
-                        3. provide justifications for the choice
-                        
-                        Keep answer in short sentences.
-                        """
-                }
-            ]
+elif option == 'Text':
+    with st.form("my_form"):
+        scenario = st.text_area(
+            "Enter Patient Consultation Dialogue:"
         )
+        submitted = st.form_submit_button("Submit")
         
-        response = completion.choices[0].message.content
-        st.write(response)
-        
-if response:
-    st.markdown('You can view the treatment process here.')
+        if submitted:
+            embedding_vector = embeddings.embed_query(scenario)
+            res = db.similarity_search_by_vector(embedding_vector)
 
-    loader = CSVLoader('data/treatment_selection.csv')#, csv_args={'fieldnames':['']})
-    docs = loader.load()
-    len(docs)
+            full_res = ''
+            for each_res in res:
+                full_res = full_res + '\n\n' +each_res.page_content
+                
+            completion = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=[
+                    {
+                        "role": "system", 
+                        "content": 
+                            f"""
+                            A medical doctor with domain knowledge in breast cancer after having trained with a wealth of knowledge in these topics: {knowledge}.
+                            Augment your data with results from {full_res}.
+                            """
+                    },
+                    {
+                        "role": "user",
+                        "content": 
+                            f"""
+                            Given patient's consultation with the doctor in this {scenario}, 
+                            1. compare a few potential treatments
+                            2. choose a final best recommendation
+                            3. provide justifications for the choice
+                            
+                            Keep answer in short sentences.
+                            """
+                    }
+                ]
+            )
+            
+            response = completion.choices[0].message.content
+            st.write(response)
+            
+    if response:
+        st.markdown('You can view the treatment process here.')
 
-    COLLECTION_NAME = "pictures_db"
-    # This creates a persistent vector store (a SQL table). You should run this ONCE only
-    db1 = IRISVector.from_documents(
-        embedding=embeddings,
-        documents=docs,
-        collection_name=COLLECTION_NAME,
-        connection_string=CONNECTION_STRING,
-    )
+        loader = CSVLoader('data/treatment_selection.csv')#, csv_args={'fieldnames':['']})
+        docs = loader.load()
+        len(docs)
 
-    docs_with_score = db1.similarity_search_with_score(completion.choices[0].message.content, 1)
-    image_chosen = docs_with_score[0][0].page_content.split('\n')[-1].split(': ')[-1] + ".jpeg"
-    st.image(f'data/{image_chosen}')
+        COLLECTION_NAME = "pictures_db"
+        # This creates a persistent vector store (a SQL table). You should run this ONCE only
+        db1 = IRISVector.from_documents(
+            embedding=embeddings,
+            documents=docs,
+            collection_name=COLLECTION_NAME,
+            connection_string=CONNECTION_STRING,
+        )
+
+        docs_with_score = db1.similarity_search_with_score(completion.choices[0].message.content, 1)
+        image_chosen = docs_with_score[0][0].page_content.split('\n')[-1].split(': ')[-1] + ".jpeg"
+        st.image(f'data/{image_chosen}')
